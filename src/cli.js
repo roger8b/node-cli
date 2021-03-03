714ee64f7ee2b1f5c78fe2a2b119be1a91703ed0
roger.silva <roger.8b@gmail.com>
2021-03-02 23:27:57 -0300

    Step-5

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import { Select } from 'enquirer'
 import git from '../src/github'
 
 const program = new Command();
@@ -6,10 +7,21 @@ program.version('0.0.1');
 
 let options = program.option('-u , --user <user_name>', 'github user name');
 
+async function questions(params) {
+    const prompt = new Select({
+        name: 'repositories',
+        message: 'Select repository',
+        choices: params
+    })
+    return await prompt.run();
+}
+
 export async function cli(args) {
     let options = program
         .parse(args)
         .opts();
     let repositories = await git.listUserRepositories(options.user);
-    console.log(repositories);
+    let repo = await questions(repositories);
+    let result = await git.getRepositoryData(options.user, repo);
+    console.log(result);
 }
